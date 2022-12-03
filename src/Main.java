a238f7fe12674ae17b483d4479e1ddfffba0911a
Vitaliy <vitalijpupcenko@gmail.com>
2022-12-03 17:05:30 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("My name is Vitaliy");
+        int a=0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Variable a= "+ a);
+            a++;
+        }
     }
 }
